a053b59d6db0c1f6f43095ac908ac090b1ac13b2
Menezesvm <vinicius.menezes2@ufrpe.br>
2026-09-09 19:23:46 -0200

    Project created

---
+++ src/test/java/com/estudosjava/developer/DeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudosjava.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
