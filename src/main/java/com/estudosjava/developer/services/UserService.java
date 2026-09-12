3feb5aa3fffc4d25a6d50075e10904a5799dcee2
Menezesvm <vinicius.menezes2@ufrpe.br>
2026-09-12 14:02:04 -0200

    Exception handling - update

--- src/main/java/com/estudosjava/developer/services/UserService.java
+++ src/main/java/com/estudosjava/developer/services/UserService.java
@@ -13,6 +13,8 @@ import com.estudosjava.developer.repositories.UserRepository;
 import com.estudosjava.developer.services.exceptions.DatabaseException;
 import com.estudosjava.developer.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,10 +45,15 @@ public class UserService {
 	}
 	
 	public User update(Long id , User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-	}
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+			}
+		}
+	
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
