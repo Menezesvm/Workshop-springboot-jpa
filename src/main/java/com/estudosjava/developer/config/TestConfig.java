1018160526aa21d40ceccedd4820a2bf392d87c9
Menezesvm <vinicius.menezes2@ufrpe.br>
2026-09-11 22:33:26 -0200

    Payment, one-to-one association

--- src/main/java/com/estudosjava/developer/config/TestConfig.java
+++ src/main/java/com/estudosjava/developer/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.estudosjava.developer.entities.Category;
 import com.estudosjava.developer.entities.Order;
 import com.estudosjava.developer.entities.OrderItem;
+import com.estudosjava.developer.entities.Payment;
 import com.estudosjava.developer.entities.Product;
 import com.estudosjava.developer.entities.User;
 import com.estudosjava.developer.entities.enums.OrderStatus;
@@ -84,6 +85,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 				
 	}
 
